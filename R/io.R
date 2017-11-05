52f143aa3c872b6932912e0e4f749a98dee1727d
Elad Chomsky <elad.chomsky@weizmann.ac.il>
2017-11-05 18:45:33 +0200

    Replaced fread_sparse() and fwrite_sparse() with fread_mm() and fwrite_mm()

--- R/io.R
+++ R/io.R
@@ -29,62 +29,145 @@ fread_rownames <- function(..., row.var='rowname')
 
 
 ########################################################################
-fwrite_sparse <- function(x, fname)
+fwrite_mm <- function(x, fname, sep=' ', row.names=TRUE, col.names=TRUE)
 {
 	ext <- tools::file_ext(fname)
-	fname <- tools::file_path_sans_ext(fname)
+	base <- tools::file_path_sans_ext(fname)
 
-    if (!is.null(rownames(x))) {
+	rows <- NULL
+	cols <- NULL
+
+	dims <- dim(x)
+	if (length(dims) != 2) {
+		stop("fwrite_mm() can only write two-dimensional metrices")
+	}
+
+	if (!is.null(rownames(x)) && ((row.names==TRUE) || is.character(row.names))) {
+		if (row.names==TRUE) {
+			row.names <- paste0(base, '.rownames.', ext)
+		}
         rows <- tibble(rowname=rownames(x))
-        data.table::fwrite(rows, paste0(fname, '.rownames.', ext), row.names=FALSE, col.names=TRUE)
         rownames(x) <- NULL
     }
 
-    if (!is.null(colnames(x))) {
+    if (!is.null(colnames(x)) && ((col.names==TRUE) || is.character(col.names))) {
+		if (col.names == TRUE) {
+			col.names <- paste0(base, '.colnames.', ext)
+		}
         cols <- tibble(colname=colnames(x))
-        data.table::fwrite(cols, paste0(fname, '.colnames.', ext), row.names=FALSE, col.names=TRUE)
+        # data.table::fwrite(cols, col.names, row.names=FALSE, col.names=FALSE)
         colnames(x) <- NULL
     }
 
-	dims <- tibble(dims=dim(x))
-	data.table::fwrite(dims, paste0(fname, '.dims.', ext), row.names=FALSE, col.names=TRUE)
+	x <- broom::tidy(x)
 
-    x <- broom::tidy(x)
-    if (any(colnames(x) != c('row', 'column', 'value'))) {
+	if (any(colnames(x) != c('row', 'column', 'value'))) {
         stop("Failed to convert argumant 'x' into a row-column-value format")
     }
+	if (mode(x$column) != 'numeric') {
+		stop("fwrite_mm() can only write numerical matrices")
+	}
 
-    data.table::fwrite(x, paste0(fname, '.', ext), sep='\t', row.names=FALSE, col.names=TRUE)
+	x <- arrange(x, row, column)
+
+	output <- file(fname, open='w')
+	writeLines('%%MatrixMarket matrix coordinate real general', output)
+	writeLines(paste(dims[1], dims[2], nrow(x), sep=sep), output)
+	close(output)
+
+	data.table::fwrite(x, fname, sep=sep, row.names=FALSE, col.names=FALSE, append=TRUE)
+
+	if (!is.null(rows)) {
+		data.table::fwrite(rows, row.names, row.names=FALSE, col.names=FALSE)
+	}
+	if (!is.null(cols)) {
+		data.table::fwrite(cols, col.names, row.names=FALSE, col.names=FALSE)
+	}
 }
 
 
 ########################################################################
-fread_sparse <- function(fname)
+fread_mm <- function(fname, sep=' ', row.names=TRUE, col.names=TRUE)
 {
 	ext <- tools::file_ext(fname)
-	fname <- tools::file_path_sans_ext(fname)
+	base <- tools::file_path_sans_ext(fname)
 
-    x <- data.table::fread(paste0(fname, '.', ext), sep='\t', header=TRUE, data.table=FALSE)
-	dims <- data.table::fread(paste0(fname, '.dims.', ext), sep='\t', header=TRUE, data.table=FALSE)
+	if (row.names==TRUE) {
+		row.names <- paste0(base, '.rownames.', ext)
+	}
+	if (col.names == TRUE) {
+		col.names <- paste0(base, '.colnames.', ext)
+	}
 
-    if (any(colnames(x) != c('row', 'column', 'value'))) {
-        stop("File '", fname, "' does not match sparse matrix format")
+	input <- file(fname, open='r')
+
+	header <- readLines(input, n=1, warn=FALSE)
+	if (sep == ' ') {
+		header <- strsplit(header, ' +')[[1]]
+	}
+	else {
+		header <- strsplit(header, sep, fixed=TRUE)[[1]]
+	}
+	if (header[1] != '%%MatrixMarket') {
+		close(input)
+		stop("File does not contain a MatrixMarket header")
+	}
+	if ((header[2] != 'matrix') || (header[3] != 'coordinate') || (header[5] != 'general')) {
+		close(input)
+		stop("fread_mm() can only read matrices in general, coordinate format")
+	}
+
+	VALUE_TYPES <- c('real'='numeric', 'integer'='integer')
+	value_type <- VALUE_TYPES[header[4]]
+	names(value_type) <- NULL
+	if (is.na(value_type)) {
+		close(input)
+		stop("File contains an unsupported value type: ", header[4])
+	}
+
+	skip <- 1
+	while(TRUE) {
+		line = readLines(input, n=1, warn=FALSE)
+		skip <- skip + 1
+		if (!startsWith(line, '%')) {
+			break
+		}
+	}
+
+	if (sep == ' ') {
+		dims <- strsplit(line, ' +')[[1]]
+	} else {
+		dims <- strsplit(line, sep, fixed=TRUE)[[1]]
+	}
+	dims <- as.numeric(dims)
+	if (length(dims) != 3) {
+		close(input)
+		stop("Could not parse matrix dimenstions line")
+	}
+	if (any(as.integer(dims) != dims)) {
+		close(input)
+		stop("Dimensions line contains non-integer values")
+	}
+	close(input)
+
+	x <- data.table::fread(fname, sep=sep, header=FALSE, skip=skip, colClasses=c('integer', 'integer', value_type), data.table=FALSE)
+	if (nrow(x) != dims[3]) {
+		stop("Number of data lines in file does not match dimensions line")
+	}
+
+	x <- Matrix::sparseMatrix(i=x[,1], j=x[,2], x=x[,3], dims=dims[1:2])
+
+	if (file.exists(row.names)) {
+        rows <- data.table::fread(row.names, sep='\t', header=FALSE, data.table=FALSE)
+        rownames(x) <- rows[,1]
     }
-    x <- Matrix::sparseMatrix(i=x$row, j=x$column, x=x$value, dims=dims$dims)
 
-    rows_fname <- paste0(fname, '.rownames.', ext)
-    if (file.exists(rows_fname)) {
-        rows <- data.table::fread(rows_fname, sep='\t', header=TRUE, data.table=FALSE)
-        rownames(x) <- rows$rowname
+    if (file.exists(col.names)) {
+        cols <- data.table::fread(col.names, sep='\t', header=FALSE, data.table=FALSE)
+        colnames(x) <- cols[,1]
     }
 
-    cols_fname <- paste0(fname, '.colnames.', ext)
-    if (file.exists(cols_fname)) {
-        cols <- data.table::fread(cols_fname, sep='\t', header=TRUE, data.table=FALSE)
-        colnames(x) <- cols$colname
-    }
-
-    return(x)
+	return(x)
 }
 
 
