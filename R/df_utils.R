d07a3c0cb1c361f8a2b486a4cde5ea8355a75abf
Elad Chomsky <elad.chomsky@weizmann.ac.il>
2018-01-18 14:17:32 +0200

    Added: segment_by(), annotate_members()

--- R/df_utils.R
+++ R/df_utils.R
@@ -1,4 +1,6 @@
 ########################################################################
+# A wrapper for matrixStats::rowMaxs()
+# Returns the maximal numeric value per row in a matrix or a dataframe
 #' @export
 rowMaxs <- function (x, rows = NULL, cols = NULL, na.rm = FALSE, dim. = dim(x), ...)
 {
@@ -24,6 +26,8 @@ rowMaxs.data.frame <- function(x, ...)
 
 
 ########################################################################
+# A wrapper for matrixStats::rowMins()
+# Returns the minimal numeric value per row in a matrix or a dataframe
 #' @export
 rowMins <- function (x, rows = NULL, cols = NULL, na.rm = FALSE, dim. = dim(x), ...)
 {
@@ -49,6 +53,8 @@ rowMins.data.frame <- function(x, ...)
 
 
 ########################################################################
+# Clips numeric values so that they lie in the range [min_val, max_val]
+# Works on both numeric arrays and dataframes
 #' @export
 clip_vals <- function(x, min_val=NULL, max_val=NULL)
 {
@@ -81,4 +87,30 @@ clip_vals.data.frame <- function(x, min_val=NULL, max_val=NULL)
         x[cols] <- clip_vals.numeric(x[cols], min_val, max_val)
     }
     return(x)
+}
+
+
+########################################################################
+# Segment a datafrane according to consecutive identical values of a
+# column expression.
+#' @export
+segment_by <- function(df, column, var='segment')
+{
+    rle_rep <- function(vals)
+    {
+        enc <- rle(vals)
+        return(rep(1:length(enc$lengths), enc$lengths))
+    }
+
+    column <- rlang::enquo(column)
+    
+    temp_df <- tibble(group=dplyr::group_indices(df), 
+                      col=df %>% dplyr::mutate(.temp_col_=!!column) %>% dplyr::pull(.temp_col_))
+    temp_df <- temp_df %>%
+               dplyr::mutate(segment=dplyr::if_else(!is.na(col), rle_rep(col), 0L)) %>%
+               dplyr::group_by(group, segment)
+
+    df[,var] <- dplyr::dense_rank( dplyr::if_else(!is.na(temp_df$col), dplyr::group_indices(temp_df), 0L) ) - 1
+
+    return(df)
 }
