a89584f849d95f2fc23cce66f756bf677ff124c7
Elad Chomsky <elad.chomsky@weizmann.ac.il>
2018-04-03 14:50:51 +0300

    Removed functions for reading/writing HDF5 (to avoid dependency on hdf5 libs)

--- R/tgutil-package.R
+++ R/tgutil-package.R
@@ -2,12 +2,6 @@
 #'
 #' @name tgutil
 #' @docType package
-#' @useDynLib tgutil
 #' @import tibble
-#' @importFrom Rcpp sourceCpp
 #' @importFrom magrittr %>%
-
-########################################################################
-.onUnload <- function (libpath) {
-  library.dynam.unload("tgutil", libpath)
-}
+NULL
