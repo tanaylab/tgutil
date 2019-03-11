f98e263d1176d3ff7911d18a9a22753efcbda7f2
Aviezer Lifshitz <aviezer.lifshitz@weizmann.ac.il>
2019-03-11 09:50:55 +0200

    added cutree_order function

--- R/utils.R
+++ R/utils.R
@@ -20,9 +20,47 @@ annotate_members <- function(x, ...)
 }
 
 
+########################################################################
 #' Install packages from the Tanay Group repository
 #' 
-#' @inheritDotParams install.packages
+#' @inheritDotParams utils::install.packages
 #' @export
 tg_install <- function(...) install.packages(..., repos=c(getOption('repos'), 'https://tanaylab.bitbucket.io/repo'))
 
+########################################################################
+#' Cut a tree into groups of data in the order of the tree
+#' 
+#' Cut a tree (result from hclust) into groups of data. Groups are in the order of the tree leafs
+#' 
+#' @param tree an hclust object 
+#' @param k the desired number of groups
+#' @param h height where the tree is to be cut 
+#' 
+#' @section Notes: Taken from http://larmarange.github.io/JLutils/reference/cutree.order.html
+#' 
+#' @examples 
+#' 
+#' hc <- hclust(dist(USArrests))
+#' memb <- cutree_order(hc, k = 10)
+#' 
+#' @export
+cutree_order <- function(tree, k = k, h = h){
+    coupe <- cutree(tree, k = k, h = h)
+    coupe.or <- coupe[tree$order]
+    coupe.out <- rep(NA, length(coupe))
+    j <- 1
+    k <- coupe.or[1]
+    for (i in 1:length(coupe)) {
+        if (coupe.or[i] == k) {
+            next
+        }
+        else {
+            coupe.out[which(coupe == k)] <- j
+            j <- j + 1
+            k <- coupe.or[i]
+        }
+    }
+    coupe.out[is.na(coupe.out)] <- j
+    names(coupe.out) <- names(coupe)
+    coupe.out
+}
